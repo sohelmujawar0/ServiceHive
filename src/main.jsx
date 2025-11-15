708bbd728f2cc14a909112afe586662cb9fc1c32
sohelmujawar0 <sohelmujawar099@gmail.com>
2025-11-15 19:23:50 +0530

    fixed absolute path import issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "C:/Users/SC/Home-maintince-service/src/index.css"; // 👈 very important
+import "./index.css"; // 👈 very important
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
